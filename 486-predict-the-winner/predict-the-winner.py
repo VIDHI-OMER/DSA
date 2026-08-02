7ef61f1c86ca8c2b1d036d4bf8eb21939f2de59e
VIDHI-OMER <155218210+VIDHI-OMER@users.noreply.github.com>
2026-08-02 13:02:35 +0530

    Time: 1 ms (65.23%) | Memory: 19.6 MB (38.07%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -2,14 +2,18 @@ class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         n=len(nums)
         tot=sum(nums)
+        memo=[[-1]*n for _ in range(n)]
         def sol(i,j):
             if(i>j):
                 return 0
             if(i==j):
                 return nums[i]
+            if memo[i][j]!=-1:
+                return memo[i][j]
             takei=nums[i]+min(sol(i+2,j),sol(i+1,j-1))
             takej=nums[j]+min(sol(i,j-2),sol(i+1,j-1))
-            return max(takei,takej)
+            memo[i][j]= max(takei,takej)
+            return memo[i][j]
         p1=sol(0,n-1)
         p2=tot-p1
         return p1>=p2
