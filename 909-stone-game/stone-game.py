19505cd0d8595ea34905818b2e5f26c539a9b6e6
VIDHI-OMER <155218210+VIDHI-OMER@users.noreply.github.com>
2026-08-02 14:22:39 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (99.37%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,19 +1,4 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        n=len(piles)
-        tot=sum(piles)
-        memo=[[-1]*n for _ in range(n)]
-        def sol(i,j):
-            if(i>j):
-                return 0
-            if(i==j):
-                return piles[i]
-            if(memo[i][j]!=-1):
-                return memo[i][j]
-            takei=piles[i]+min(sol(i+2,j),sol(i+1,j-1))
-            takej=piles[j]+min(sol(i,j-2),sol(i+1,j-1))
-            memo[i][j]=max(takei,takej)
-            return memo[i][j]
-        p1=sol(0,n-1)
-        p2=tot-p1
-        return p1>p2        
+        return True
+        
