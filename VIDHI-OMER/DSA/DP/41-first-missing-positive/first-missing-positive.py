11b51646a8f652ebfb979c41067a74dce030a880
VIDHI-OMER <155218210+VIDHI-OMER@users.noreply.github.com>
2026-08-11 10:43:16 +0530

    Time: 19 ms (88.63%) | Memory: 34.8 MB (21.28%) - LeetSync

---
+++ VIDHI-OMER/DSA/DP/41-first-missing-positive/first-missing-positive.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        s=set(nums)
+        for i in range(1,len(nums)+2):
+            if i not in s:
+                return i
+
+        
