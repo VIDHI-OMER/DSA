5deb0aea9816bf2613ce500a5e1b730b86d74353
VIDHI-OMER <155218210+VIDHI-OMER@users.noreply.github.com>
2026-09-25 12:58:35 +0530

    Time: 2787 ms (31.51%) | Memory: 19.5 MB (25.21%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        maxi=0
+        def check(f):
+            c=0
+            for i in range(26):
+                if f[i]==0:
+                    continue
+                if c==0:
+                    c=f[i]
+                elif f[i]!=c:
+                    return False
+            return True
+
+        for i in range(len(s)):
+            f=[0]*26
+            for j in range(i,len(s)):
+                f[ord(s[j])-ord('a')]+=1
+                if check(f):
+                    maxi=max(maxi,j-i+1)
+        return maxi
+                
